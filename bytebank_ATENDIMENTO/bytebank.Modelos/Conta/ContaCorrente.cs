678edad2d978ca06fbf05aedd4845fa7285727c7
guusluiz <gustavoluizmiranda405@gmail.com>
2022-11-28 17:26:06 -0300

    quarto commit

--- bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
+++ bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
@@ -132,12 +132,20 @@
 
         }
 
+        public ContaCorrente(int numero_agencia)
+        {
+            Numero_agencia = numero_agencia;
+            Conta = Guid.NewGuid().ToString().Substring(0, 8);
+            Titular = new Cliente();
+            TotalDeContasCriadas += 1;
+        }
+
         public static int TotalDeContasCriadas { get; set; }
 
         //public override bool Equals(object? conta)
         //{
         //    ContaCorrente outroConta = conta as ContaCorrente;
-            
+
         //    if (outroConta == null)
         //    {
         //        return false;
@@ -147,6 +155,16 @@
         //        Conta.Equals(outroConta.Conta);
         //}
 
+        public override string ToString()
+        {
 
+            return $" === DADOS DA CONTA === \n" +
+                   $"Número da Conta : {this.Conta} \n" +
+                   $"Número da Agência : {this.Numero_agencia} \n" +
+                   $"Saldo da Conta: {this.Saldo} \n" +
+                   $"Titular da Conta: {this.Titular.Nome} \n" +
+                   $"CPF do Titular  : {this.Titular.Cpf} \n" +
+                   $"Profissão do Titular: {this.Titular.Profissao}\n\n";
+        }
     }
 }
